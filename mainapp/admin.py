203e5041de9d9f8f5d1b8eef31483630d7f95b1a
Aravind2203 <aravindofficial2203@gmail.com>
2023-03-15 14:37:31 +0530

    admin changes

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Registration)
+#admin.site.register(Registration)
 admin.site.register(FifaRegistration)
 admin.site.register(CodRegistration)
 admin.site.register(ValoRegistration)
 admin.site.register(Contact)
+
+@admin.register(Registration)
+class InviteCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'number',
+        'email',
+        'college',
+        'event_name',
+        'head_name',
+        'member_1'
+    )
+    search_fields = ['email']
+    list_filter = ('event_name',)
